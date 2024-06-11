1ae3c767a1c59a6419487dfe526a0dd0d3e0e641
cho-yohan <k023g1037@g.neec.ac.jp>
2024-06-11 11:28:23 +0900

    main

--- DirectXGame/Skydome.cpp
+++
@@ -1,9 +0,0 @@
-#include "Skydome.h"
-
-void Skydome::Initialize() {}
-
-void Skydome::Update() {}
-
-void Skydome::Draw() { 
-	model_->Draw(worldTransform_, viewProjection_);
-}
--- DirectXGame/scene/GameScene.h
+++ DirectXGame/scene/GameScene.h
@@ -8,10 +8,6 @@
 #include "Sprite.h"
 #include "ViewProjection.h"
 #include "WorldTransform.h"
-#include "Player.h"
-#include "DebugCamera.h"
-
-#include <vector>
 
 /// <summary>
 /// ゲームシーン
@@ -48,25 +44,6 @@ private: // メンバ変数
 	DirectXCommon* dxCommon_ = nullptr;
 	Input* input_ = nullptr;
 	Audio* audio_ = nullptr;
-
-	uint32_t textureHandel_ = 0;
-
-	Model* model_ = nullptr;
-	Model* modelBlock_ = nullptr;
-	Model* modelSkydome_ = nullptr;
-
-	Player* player_ = nullptr;
-
-	ViewProjection viewProjection_;
-	WorldTransform worldTransform_;
-
-	std::vector<std::vector<WorldTransform*>> worldTransformBlocks_;
-
-	// デバッグカメラ
-	DebugCamera* debugCamera_ = nullptr;
-
-	// デバッグカメラ有効
-	bool isDebugCameraActive_ = false;
 
 	/// <summary>
 	/// ゲームシーン用
