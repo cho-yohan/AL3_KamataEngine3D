601761be29760de4d92fe61d073d98062e800d04
cho-yohan <k023g1037@g.neec.ac.jp>
2024-06-25 13:16:51 +0900

    02_05_確認課題

--- DirectXGame/Player.h
+++ DirectXGame/Player.h
@@ -1,10 +1,16 @@
 #include "Model.h"
 #include "WorldTransform.h"
 #include "ViewProjection.h"
+#include "MathUtilityForText.h"
+
+enum class LRDirection {
+	kRight,
+	kLeft,
+};
 
 class Player { 
 public: 
-	void Initialize(Model* model, uint32_t textureHandle, ViewProjection* viewProjection);
+	void Initialize(Model* model, ViewProjection* viewProjection, const Vector3& position);
 
 	void Update();
 
@@ -17,4 +23,29 @@ private:
 	Model* model_ = nullptr;
 
 	uint32_t textureHandle_ = 0u;
+
+	Vector3 velocity_ = {};
+
+	static inline const float kAcceleration = 5;
+	static inline const float kAttenuation = 2;
+	static inline const float kLimitRunSpeed = 4;
+
+	LRDirection lrDirection_ = LRDirection::kRight;
+
+	float turnFirstRotationY_ = 0.0f;
+	float turnTimer_ = 0.0f;
+
+	static inline const float kTimeTurn = 0.3f;
+
+	//接地状態フラグ
+	bool onGround_ = true;
+	// 着地フラグ
+	bool landing;
+
+	//重力加速度 (下方向)
+	static inline const float kGravityAcceleration = 3;
+	//最大落下速度 (下方向)
+	static inline const float kLimitFallSpeed = 3;
+	//ジャンプ初速 (上方向)
+	static inline const float kJumpAcceleration = 3;
 };
