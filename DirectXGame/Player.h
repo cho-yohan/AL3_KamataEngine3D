d7175514c80d88cadff71b5afc5832af1053c381
cho-yohan <k023g1037@g.neec.ac.jp>
2024-07-05 11:52:07 +0900

    02_07_確認課題

--- DirectXGame/Player.h
+++ DirectXGame/Player.h
@@ -5,6 +5,8 @@
 #include "WorldTransform.h"
 #include "myMath.h"
 
+class MapChipField;
+
 /// <summary>
 /// 自キャラ
 /// </summary>
@@ -35,6 +37,8 @@ public:
 	const WorldTransform& GetWorldTransform() const { return worldTransform_; }
 
 	const Vector3& GetVelocity() const { return velocity_; }
+
+	void SetMapChipField(MapChipField* mapChipField) { mapChipField_ = mapChipField; }
 
 private:
 	// ワールド変換データ
@@ -73,4 +77,11 @@ private:
 	static inline const float kLimitFallSpeed = 0.2f;
 	// ジャンプ初速（上方向）
 	static inline const float kJumpAcceleration = 0.7f;
+
+	// マップチップによるフィールド
+	MapChipField* mapChipField_ = nullptr;
+
+	// キャラクターの当たり判定サイズ
+	static inline const float kWidth = 0.8f;
+	static inline const float kHeight = 0.8f;
 };
